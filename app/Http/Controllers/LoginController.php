84176fcb119a8de16a9e63e9105f0109fedbea42
Đà Dương Xuân <dadx.jsoft@gmail.com>
2021-05-25 22:30:47 +0700

    initial commit

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+class LoginController extends Controller
+{
+    public function login()
+    {
+        return view('login');
+    }
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+}
---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Responsive Admin &amp; Dashboard Template based on Bootstrap 5">
+    <meta name="author" content="AdminKit">
+    <meta name="keywords" content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="shortcut icon" href="{{ asset('static/img/icons/icon-48x48.png') }}" />
+
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js"
+            integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous">
+    </script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js"integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous">
+    </script>
+
+    <title>Blank Page | AdminKit Demo</title>
+
+    <link href="{{ asset('static/css/app.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+</head>
+
+<body>
+<div class="wrapper">
+    @include('admin.partial.sidebar')
+
+
+    <div class="main">
+        @include('admin.partial.navbar')
+
+        <main class="content">
+            <div class="container-fluid p-0">
+
+                @yield('content')
+
+            </div>
+        </main>
+
+        @include('admin.partial.footer')
+    </div>
+</div>
+
+<script src="{{ asset('static/js/app.js') }}"></script>
+
+</body>
+
+</html>
